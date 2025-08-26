cc5a73a35ed2d0cdfb6cdc2a50179e574876080b
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2025-08-26 15:14:11 +0530

    remove some data

--- run-mvn.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Set the correct JAVA_HOME for this project
-set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.16.8-hotspot
-
-REM Display the JAVA_HOME being used
-echo Using JAVA_HOME: %JAVA_HOME%
-
-REM Run Maven with the correct Java version
-mvn %*
